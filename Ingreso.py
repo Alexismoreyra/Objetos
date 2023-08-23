25c351d4b3ace211d629bf3a3d872c0c216b4ed6
Alexismoreyra <112131757+Alexismoreyra@users.noreply.github.com>
2023-08-23 20:38:08 -0300

    Add files via upload

--- Ingreso.py
+++ Ingreso.py
@@ -29,3 +29,19 @@ class Ingreso():
         for lista in listaUsuario:
             if dni==lista.dni:
                 listaUsuario.remove(object)
+
+    def modificaciones (self,deposito,retiro):
+        return'''
+        deposito: (deposito)
+        retiro: (retiro)'''
+
+    def editarSaldo(self):
+        dni=input('dni a editar')
+        for lista in listaUsuario:
+            if dni==Usuario.dni:
+                deposito=int(input(''))
+                retiro=int(input(''))
+                Usuario.editarSaldo(deposito,retiro)
+                mod=Usuario.modificaciones(deposito;retiro)
+                Usuario.historial.append(mod)
+
