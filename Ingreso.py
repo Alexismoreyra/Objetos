453d5ad829f0aa81c729980dde65157ceba59c33
Alexismoreyra <112131757+Alexismoreyra@users.noreply.github.com>
2023-08-23 20:05:50 -0300

    Add files via upload

---
+++ Ingreso.py
@@ -0,0 +1,32 @@
+from Usuario import Usuario
+
+listaUsuario = []
+
+class Ingreso():
+    def agregar (self):
+        prit('registro de usuario')
+        dni=input('dni: ')
+        nombre=input('nombre: ')
+        apellido=input('apellido: ')
+        deposito=int(input('deposito: '))
+        retiro=int(input('retiro: '))
+        objeto=Usuario(dni,nombre,apellido,deposito,retiro)
+        listaUsuario.append(objeto)
+
+    def listar(self):
+        print('lista de usuario')
+        for objeto in listaUsuario:
+            objeto.imprimir()
+
+    def consulta(self):
+        dni=input('dni: ')
+        for lista in listaUsuario:
+            if dni==lista.dni:
+                lista.imprimir()
+
+    def eliminar (self)
+        dni=input('dni: ')
+        for lista in listaUsuario:
+            if dni==lista.dni:
+                listaUsuario.remove(object)
+
